4d5f79f76885338fb79e893fb01fdc1eb78c3187
alt-0191 <2223147307@qq.com>
2026-03-09 18:29:22 +0800

    Erase settings and reboot to UF2 on start

--- app/src/settings/reset_settings_on_start.c
+++ app/src/settings/reset_settings_on_start.c
@@ -5,9 +5,21 @@
  */
 
 #include <zephyr/init.h>
+#include <zephyr/sys/reboot.h>
+#include <dt-bindings/zmk/reset.h>
 
 #include <zmk/settings.h>
 
+static int zmk_settings_erase_and_reboot_to_bootloader(void) {
+    int ret = zmk_settings_erase();
+    if (ret == 0) {
+        // Reboot into UF2 bootloader so user can flash normal firmware
+        sys_reboot(RST_UF2);
+    }
+    return ret;
+}
+
 // Reset after the kernel is initialized but before any application code to
 // ensure settings are cleared before anything tries to use them.
-SYS_INIT(zmk_settings_erase, POST_KERNEL, CONFIG_ZMK_SETTINGS_RESET_ON_START_INIT_PRIORITY);
+SYS_INIT(zmk_settings_erase_and_reboot_to_bootloader, POST_KERNEL,
+         CONFIG_ZMK_SETTINGS_RESET_ON_START_INIT_PRIORITY);
